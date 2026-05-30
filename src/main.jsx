77f9ce6256fcfa1c6f4dd0f49b21ca527c851f1e
fazilathafif <fazilathafif@users.noreply.github.com>
2026-05-30 20:54:58 +0530

    feat: add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { inject } from '@vercel/analytics'
 import App from './App.jsx'
 import './styles/global.css'
+
+inject()
 
 // When a new service worker takes control, reload to get fresh JS.
 if ('serviceWorker' in navigator) {
